de53270201037b1345bb29fffd1918deb633c999
John Cordero <johncorderox@gmail.com>
2018-02-27 00:43:17 +0000

    model validations and custom functions

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,6 @@
 class Bid < ActiveRecord::Base
   belongs_to :user
   belongs_to :auction
+
+  validates :price, presence: true, numericality: true
 end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,4 +2,6 @@ class Delivery < ActiveRecord::Base
   belongs_to :users
   belongs_to :address
   belongs_to :auction
+
+  validates :price, numericality: true, presence: true
 end
