462cfe244651ed36940c5a8e5a12f8a7cce3af28
Joy Kim <silverxrain125@gmail.com>
2018-03-02 00:24:11 -0800

    added Truck& logo to all html files and navbar brand on application scss file

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ApplicationMailer
-  default from: "eunjkim1221@naver.com"
+  default from: "eunjkim1221@yahoo.com"
   def signup_confirmation(subscribe)
     @subscribe = subscribe
-
     mail to: @subscribe.email , subject: "Sign up confirmation"
   end
 end
